4bd0b0dc3f10fa821c81f3f0761726de83acc875
Constan <rafael.constantino89@gmail.com>
2022-09-03 00:02:38 -0300

    tela 2 faltando a lógica do jogo

--- script.js
+++ script.js
@@ -43,6 +43,7 @@ function renderizarQuizz(resposta){
     const listaPerguntas = document.querySelector(".main");
     let divDePerguntas = '';
 
+    //laço para adicionar as perguntas
     for( i = 0; i < perguntas.length; i++) {
         divDePerguntas += `
         <div class="perguntas-quizz">       
@@ -52,16 +53,21 @@ function renderizarQuizz(resposta){
         `
         respostas = perguntas[i].answers;
 
-        console.log(respostas);
+        //respostas organizadas aleatoriamente
+        respostas.sort(comparador);
 
-        // const listaRespostas = document.querySelector(".perguntas-quizz");
-
+        function comparador() { 
+            return Math.random() - 0.5; 
+        }
+        
+        //laço para adicionar as respostas
         for( i2 = 0; i2 < respostas.length; i2++){
             divDePerguntas += `
             
-                <div class="alt">
+                <div class="alt${i}" onclick="selecionarAlt${i}(this)">
                     <img src="${respostas[i2].image}">
                     <p>${respostas[i2].text}</p>
+                    <div class="true-false">${respostas[i2].isCorrectAnswer}</div>
                 </div>
             
             `
@@ -72,10 +78,119 @@ function renderizarQuizz(resposta){
         </div>
         `
         listaPerguntas.innerHTML = divDePerguntas;
-        console.log(divDePerguntas);
-    }
+        //console.log(divDePerguntas);
 
     
+    }
+       
+}
+
+function selecionarAlt0(altSelecionada){
+    const selecionarTodas = document.querySelectorAll(".alt0");
+
+    // console.log(selecionarTodas);
+
+    altSelecionada.classList.add ('selecionada');
+
+    for( i = 0; i < 4; i++){
+        if(selecionarTodas[i].classList.contains('selecionada') === false){
+            selecionarTodas[i].style.opacity = ('0.5');
+        }
+    }
+
+    const certoOuErrado = document.querySelectorAll(".alt0>p");
+
+    for( i = 0; i < 4; i++){
+        if(respostas[i].isCorrectAnswer === true){
+            certoOuErrado[i].style.color = ('green');
+        } else {
+            certoOuErrado[i].style.color = ('red');
+
+        }
+    }
     
 }
+
+function selecionarAlt1(altSelecionada){
+    const selecionarTodas = document.querySelectorAll(".alt1");
+
+    // console.log(selecionarTodas);
+
+    altSelecionada.classList.add ('selecionada');
+
+    for( i = 0; i < 4; i++){
+        if(selecionarTodas[i].classList.contains('selecionada') === false){
+            selecionarTodas[i].style.opacity = ('0.5');
+        }
+    }
+
+    const certoOuErrado = document.querySelectorAll(".alt1>p");
+
+    for( i = 0; i < 4; i++){
+        if(respostas[i].isCorrectAnswer === true){
+            certoOuErrado[i].style.color = ('green');
+        } else {
+            certoOuErrado[i].style.color = ('red');
+
+        }
+    }
+    
+}
+
+function selecionarAlt2(altSelecionada){
+    const selecionarTodas = document.querySelectorAll(".alt2");
+
+    // console.log(selecionarTodas);
+
+    altSelecionada.classList.add ('selecionada');
+
+    for( i = 0; i < 4; i++){
+        if(selecionarTodas[i].classList.contains('selecionada') === false){
+            selecionarTodas[i].style.opacity = ('0.5');
+        }
+    }
+
+    const certoOuErrado = document.querySelectorAll(".alt2>p");
+
+    for( i = 0; i < 4; i++){
+        if(respostas[i].isCorrectAnswer === true){
+            certoOuErrado[i].style.color = ('green');
+        } else {
+            certoOuErrado[i].style.color = ('red');
+
+        }
+    }
+    
+}
+
+function selecionarAlt3(altSelecionada){
+    const selecionarTodas = document.querySelectorAll(".alt3");
+
+    // console.log(selecionarTodas);
+
+    altSelecionada.classList.add ('selecionada');
+
+    for( i = 0; i < 4; i++){
+        if(selecionarTodas[i].classList.contains('selecionada') === false){
+            selecionarTodas[i].style.opacity = ('0.5');
+        }
+    }
+
+    const certoOuErrado = document.querySelectorAll(".alt3>p");
+
+    for( i = 0; i < 4; i++){
+        if(respostas[i].isCorrectAnswer === true){
+            certoOuErrado[i].style.color = ('green');
+        } else {
+            certoOuErrado[i].style.color = ('red');
+
+        }
+    }
+    
+}
+
+
+
+
+
 
